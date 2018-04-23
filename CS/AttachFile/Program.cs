23197720e193f2028c9e59bee8438818ab074451
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AttachFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.Pdf;
+using System;
+using System.Text;
+
+namespace AttachFile {
+    class Program {
+        static void Main(string[] args) {
+
+            using (PdfDocumentProcessor processor = new PdfDocumentProcessor()) {
+                // Load a document.
+                processor.LoadDocument("..\\..\\Document.pdf");               
+
+                // Attach a file to the PDF document. 
+                processor.AttachFile(new PdfFileAttachment() {
+                    CreationDate = DateTime.Now,
+                    Description = "This is my attach file.",                   
+                    FileName = "MyAttach.txt",
+                    Data = GetData(),
+                });
+
+                // The attached document.
+                processor.SaveDocument("..\\..\\Result.pdf");
+            }
+        }
+
+        static byte[] GetData() {
+            string s = "this is my text";
+            byte[] data = Encoding.ASCII.GetBytes(s);
+            return data;
+        }
+    }
+}
