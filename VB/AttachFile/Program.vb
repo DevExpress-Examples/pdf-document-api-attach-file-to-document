5f38a69a1c5779a1ead7764c938782da74413a02
DevExpressExampleBot <fakeEmail>
2022-02-01 21:09:25 +0300

    Source auto update [skip ci]

--- VB/AttachFile/Program.vb
+++ VB/AttachFile/Program.vb
@@ -1,23 +1,16 @@
-﻿Imports DevExpress.Pdf
-Imports System
+Imports DevExpress.Pdf
 Imports System.IO
 
 Namespace AttachFile
-    Friend Class Program
-        Shared Sub Main(ByVal args() As String)
 
-            Using processor As New PdfDocumentProcessor()
+    Friend Class Program
+
+        Shared Sub Main(ByVal args As String())
+            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Load a document.
                 processor.LoadDocument("..\..\Document.pdf")
-
                 ' Attach a file to the PDF document. 
-                processor.AttachFile(New PdfFileAttachment() With { _
-                    .CreationDate = Date.Now, _
-                    .Description = "This is my attach file.", _
-                    .FileName = "MyAttach.txt", _
-                    .Data = File.ReadAllBytes("..\..\FileToAttach.txt") _
-                })
-
+                processor.AttachFile(New PdfFileAttachment() With {.CreationDate = Date.Now, .Description = "This is my attach file.", .FileName = "MyAttach.txt", .Data = File.ReadAllBytes("..\..\FileToAttach.txt")})
                 ' The attached document.
                 processor.SaveDocument("..\..\Result.pdf")
             End Using
