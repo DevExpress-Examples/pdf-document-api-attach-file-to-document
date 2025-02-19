18254d6afe98d593f368f8f141316445d4fdeb53
DevExpressExampleBot <fakeEmail>
2025-02-19 18:27:18 +0400

    autogenerate VB

--- VB/AttachFile/Program.vb
+++ VB/AttachFile/Program.vb
@@ -1,5 +1,5 @@
-Imports System.IO
 Imports DevExpress.Pdf
+Imports System.Diagnostics
 
 Namespace AttachFile
 
@@ -10,10 +10,11 @@ Namespace AttachFile
                 ' Load a document.
                 processor.LoadDocument("..\..\..\Document.pdf")
                 ' Attach a file to the PDF document. 
-                processor.AttachFile(New PdfFileAttachment() With {.CreationDate = Date.Now, .Description = "This is my attach file.", .FileName = "MyAttach.txt", .Data = File.ReadAllBytes("..\..\..\FileToAttach.txt")})
+                processor.AttachFile(New PdfFileAttachment() With {.CreationDate = DateTime.Now, .Description = "This is my attach file.", .FileName = "MyAttach.txt", .Data = File.ReadAllBytes("..\..\..\FileToAttach.txt")})
                 ' The attached document.
                 processor.SaveDocument("..\..\..\Result.pdf")
             End Using
+
             Process.Start(New ProcessStartInfo("..\..\..\Result.pdf") With {.UseShellExecute = True})
         End Sub
     End Class
